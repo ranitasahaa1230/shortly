e68461126ab604c650fdce8932524ba99680babe
Ranita Saha <saharanitaa1230@gmail.com>
2022-07-13 01:24:30 +0530

    feat: added navbar

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h2>Shortly</h2>
+                <a>Features</a>
+                <a>Pricing</a>
+                <a>Resources</a>
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
