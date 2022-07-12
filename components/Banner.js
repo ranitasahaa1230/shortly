72ce6c2667a6e70ae8950df50be3861206aa1026
Ranita Saha <saharanitaa1230@gmail.com>
2022-07-13 01:43:03 +0530

    feat: added meteor

---
+++ components/Banner.js
@@ -0,0 +1,13 @@
+const Banner = () => {
+    return ( 
+        <div className="banner">
+           <div className="banner-container">
+           <h1>Boost your links today</h1>
+           <button>Get Started</button>
+           </div>
+
+        </div>
+     );
+}
+ 
+export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
@@ -22,6 +23,7 @@ export default function Home() {
       
       <div className="content">
           <Navbar/>
+          <Banner/>
           <Footer/>
       </div>
     </div>
